9565f1f580031a7006cfaf13acfa4c122d24d0c2
Lucie Votypkova <lvotypko@lvotypko.brq.csb>
2018-07-03 11:24:00 +0200

    dfd

--- src/main/resources/org/jenkinsci/test/acceptance/docker/fixtures/KerberosContainer/src/setIp.sh
+++ src/main/resources/org/jenkinsci/test/acceptance/docker/fixtures/KerberosContainer/src/setIp.sh
@@ -1,5 +1,14 @@
 echo -n "ADDRESS=" > /.dockerenv
 hostname -I > /.dockerenv
 ADDRESS=`hostname -I`
+rm -f /dev/random && ln -s /dev/urandom /dev/random
+/usr/sbin/kdb5_util create -s -P "ATH"
+/usr/sbin/kadmin.local -w ATH -q "addprinc -pw ATH -clearpolicy -e des-cbc-md5:normal,des-cbc-crc:normal,rc4-hmac:normal user" &&\
+/usr/sbin/kadmin.local -w ATH -q "addprinc -pw ATH -clearpolicy -e des-cbc-md5:normal,des-cbc-crc:normal,rc4-hmac:normal HTTP/${ADDRESS}" &&\
+sed -i "s@__KDC_PORT__@88@g; s@__ADMIN_PORT__@749@g" /etc/krb5.conf &&\
+sed -i "s@_ADDRESS_@${ADDRESS}@g; s@_ADDRESS_@${ADDRESS}@g" /etc/krb5.conf
+mkdir -p /target/keytab &&\
+bash keytab.sh /target/keytab/service HTTP/${ADDRESS}@EXAMPLE.COM &&\
+bash keytab.sh /target/keytab/user user@EXAMPLE.COM
 export ADDRESS=$ADDRESS
 echo "ADDRESS=$ADDRESS" > /.dockerenv

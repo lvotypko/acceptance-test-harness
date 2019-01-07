a0f1f3da58d675213e2b01117b6b33e5ed8fbd0f
Lucie Votypkova <lvotypko@lvotypko.brq.csb>
2019-01-07 17:05:32 +0100

    Change message.

--- src/main/java/org/jenkinsci/test/acceptance/plugins/warnings/WarningsPublisher.java
+++ src/main/java/org/jenkinsci/test/acceptance/plugins/warnings/WarningsPublisher.java
@@ -5,7 +5,7 @@ import org.jenkinsci.test.acceptance.po.*;
 /**
  * @author Kohsuke Kawaguchi
  */
-@Describable("Scan for compiler warnings")
+@Describable("Record compiler warnings and static analysis results")
 public class WarningsPublisher extends AbstractStep implements PostBuildStep {
     private Control addConsoleLogScanner = control("repeatable-add");
     private Control addWorkspaceFileScanner = control("repeatable-add[1]");
